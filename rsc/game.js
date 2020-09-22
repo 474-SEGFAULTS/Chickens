13460ae1438da347ae83fe6e075920ffa179d6b6
happyaaa <happyxgao@gmail.com>
2020-09-21 21:34:07 -0400

    main_menu_screen

--- rsc/game.js
+++ rsc/game.js
@@ -4,3 +4,23 @@
  */
 
 // ..
+
+
+
+
+// Author: Happy
+// jquery start new singleGameScene/multiGameScene
+$(document).ready(function () {
+    $('#singlePlayer').hide();
+    $('#multiPlayer').hide();
+    $("#singlePlayerbtn").click(function () {
+        $('#menuScreen').fadeOut('fast', function () {
+            $('#singlePlayer').fadeIn('fast');
+        });
+    });
+    $("#multiPlayerbtn").click(function () {
+        $('#menuScreen').fadeOut('fast', function () {
+            $('#multiPlayer').fadeIn('fast');
+        });  
+    });
+});
