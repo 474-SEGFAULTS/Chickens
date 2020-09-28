3d37a2165e39a450143166154a49cc5f54576b0b
wjm2038 <10401572+wjm@users.noreply.github.com>
2020-09-28 13:45:55 -0400

    add sound and handle game input

--- rsc/game.js
+++ rsc/game.js
@@ -5,8 +5,35 @@
 
 // ..
 
+// Author: Jiamian
+// handle all the key press
 
-
+document.addEventListener('keydown', function(event){
+    if(event.key=='w' | event.key=='ArrowUp'){
+        alert('up');
+        move('up');
+    }
+    else if(event.key=='s' | event.key=='ArrowDown'){
+        alert('down');
+        move('down');
+    }
+    else if(event.key=='a' | event.key=='ArrowLeft'){
+        move('left');
+        alert('left');
+    }
+    else if(event.key=='d' | event.key=='ArrowRight'){
+        alert('right');
+        move('right');
+    }
+    else if(event.key=='e'){
+        alert('open inventory');
+    }
+});
+// Author: Jiamian
+// move the Chickens
+function move(direction){
+    //TODO
+}
 
 // Author: Happy
 // jquery start new singleGameScene/multiGameScene
--- rsc/sounds.js
+++ rsc/sounds.js
@@ -4,3 +4,22 @@
  */
 
 // ...
+//Add cursor select sound on all button
+$(document).ready(function (){
+  $('button').click(function(){
+    playSound("CursorSelect");
+  });
+});
+//TODO
+//Attach all the sounds to the html page when loading
+function addSounds() {
+  let newSrc=document.createElement("source");
+  newSrc.setAttribute('src','rsc/sounds/CursorSelect.wav');
+  newSrc.setAttribute('id','CursorSelect');
+  document.getElementById('sounds').appendChild(newSrc);
+};
+
+//Play sound by finding it's if
+function playSound(soundid){
+  document.getElementById(soundid).play();
+}
